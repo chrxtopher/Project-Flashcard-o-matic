fa8cc796035fb1a2e2725c14a46d85f4e3b72c3e
Christopher Roddy <chrxtopherxddy@gmail.com>
2021-10-26 00:12:40 -0700

    Added functionality for the list of cards to be displayed for each deck.
    Styling for each card added as well. Everything appears to be working properly.

--- src/Layout/Deck.js
+++ src/Layout/Deck.js
@@ -28,6 +28,50 @@ function Deck() {
     }
   };
 
+  const cardList = deck.cards.map((card) => {
+    return (
+      <div className="card">
+        <div className="card-body">
+          <div className="row">
+            <div className="col">
+              <p className="card-text border-right">Q: {card.front}</p>
+            </div>
+            <div className="col">
+              <p className="card-text">A: {card.back}</p>
+            </div>
+          </div>
+          <div className="mt-3 float-right">
+            <button className="btn btn-warning border border-dark mr-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                fill="currentColor"
+                class="bi bi-pen-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z" />
+              </svg>{" "}
+              Edit
+            </button>
+            <button className="btn btn-danger border border-dark">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="currentColor"
+                class="bi bi-trash-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <nav aria-label="breadcrumb">
@@ -105,6 +149,7 @@ function Deck() {
       </div>
       <div>
         <h3>Cards</h3>
+        <div>{cardList}</div>
       </div>
     </div>
   );
